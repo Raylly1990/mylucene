244d3d16879745f7aa49c10c8279259a47dca6b7
tony-wdl <18367149585@163.com>
2018-12-02 13:06:12 +0800

    添加入库类

---
+++ src/main/java/cc/wangdl/mylucene/MainEntry.java
@@ -0,0 +1,10 @@
+package cc.wangdl.mylucene;
+
+/**
+ * Description: //TODO(简略描述这个类的作用)
+ * Author: WangDL
+ * Date: 2018-12-02 13:04
+ * Copyright (c) 2018, ewell.com All Rights Reserved.
+ */
+public class MainEntry {
+}

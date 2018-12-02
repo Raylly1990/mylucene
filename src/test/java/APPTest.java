b20f604ad0448e2dcbd72c9b4006b0af479d45a5
tony-wdl <18367149585@163.com>
2018-12-02 13:16:06 +0800

    commit-提交到本地仓库

---
+++ src/test/java/APPTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Description: 测试demo类
+ * Author: WangDL
+ * Date: 2018-12-02 13:13
+ * Copyright (c) 2018, ewell.com All Rights Reserved.
+ */
+public class APPTest {
+
+    @Test
+    public void test1() {
+        System.out.println("this is  a lucene demo!!");
+    }
+}
